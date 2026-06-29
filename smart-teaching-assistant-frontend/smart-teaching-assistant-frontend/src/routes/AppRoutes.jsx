512985bdd31cf653d0e0f2f716cb08e9ce5855a4
SHRIJADHANALAKSHMI <shrijasm@gmail.com>
2026-06-29 16:13:16 +0530

    Remove duplicate frontend folder

--- smart-teaching-assistant-frontend/smart-teaching-assistant-frontend/src/routes/AppRoutes.jsx
+++ smart-teaching-assistant-frontend/smart-teaching-assistant-frontend/src/routes/AppRoutes.jsx
@@ -5,8 +5,7 @@ import Colleges from "../pages/superadmin/Colleges.jsx";
 import AI_Monitoring from "../pages/superadmin/AI Monitoring.jsx";
 import Analytics from "../pages/superadmin/Analytics.jsx";
 import InviteCollege from "../pages/superadmin/InviteCollegeAdmin.jsx";
-import AdminManagement from "../pages/superadmin/AdminManagement.jsx";
-import Reports from "../pages/superadmin/Reports.jsx";
+
 export default function AppRoutes() {
   return (
     <Routes>
@@ -16,8 +15,6 @@ export default function AppRoutes() {
       <Route path="/superadmin/ai-monitoring" element={<AI_Monitoring />} />
       <Route path="/superadmin/analytics" element={<Analytics />} />
       <Route path="/superadmin/invite-college" element={<InviteCollege />} />
-      <Route path="/superadmin/admin-management" element={<AdminManagement />} />
-      <Route path="/superadmin/reports" element={<Reports />} />
     </Routes>
   );
 }
